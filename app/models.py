5a47a5a1c687b6e0b394b0e3da5abadc95718c3b
creedtech <ayodeleayolaoke4@gmail.com>
2023-04-23 10:46:24 +0100

    reverting

--- app/models.py
+++ app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from autoslug import AutoSlugField
+from django.template.defaultfilters import slugify
+from django.core.validators import FileExtensionValidator
 # Create your models here.
+class LivingOptionsCategory(models.Model):
+   name = models.CharField(max_length=150)
+   short_description = models.TextField(max_length=200, null=True, blank=True)
+   description = models.TextField(null=True, blank=True)
+   single_image = models.ImageField(
+      upload_to='living_options/single_images',
+      max_length=500,
+      validators=[
+             FileExtensionValidator(
+            allowed_extensions=[
+               'png', 'jpg', 'jpeg', 'webp'
+            ]
+         )
+      ],
+   )
+   slug = AutoSlugField(unique=True, populate_from='name', sep='-', null=True)
+   date_created = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+      return self.name
+
+   def save(self, *args, **kwargs):
+      self.slug = slugify(self.name)
+      super().save(*args, **kwargs)
+
+   class Meta:
+      verbose_name_plural = "LivingOptions Categories"
+
+class LivingOptions(models.Model):
+   title = models.CharField(max_length=255)
+   description = models.TextField(null=True, blank=True)
+   image = models.ImageField(
+      upload_to='living_options/images',
+      max_length=500,
+      validators=[
+             FileExtensionValidator(
+            allowed_extensions=[
+               'png', 'jpg', 'jpeg', 'webp'
+            ]
+         )
+      ],
+   )
+   category = models.ForeignKey(LivingOptionsCategory, on_delete=models.CASCADE)
+   date_posted = models.DateTimeField(auto_now_add=True)
+   last_updated = models.DateField(auto_now=True)
+   slug = AutoSlugField(unique=True, populate_from='title', sep='-', null=True)
+
+   def __str__(self):
+      return f'{self.title} on {self.date_posted}'
+      
+   def save(self, *args, **kwargs):
+      self.slug = slugify(self.title)
+      super().save(*args, **kwargs)
--- app/views.py
+++ app/views.py
@@ -1,29 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from app.models import LivingOptions
+from django.views import View
 
 # Create your views here.
+
+
 def index(request):
     template_name = 'pages/index.html'
-    return render(request,template_name)
+    return render(request, template_name)
+
 
 def about(request):
     template_name = 'pages/about.html'
-    return render(request,template_name)
+    return render(request, template_name)
+
 
 def contact(request):
     template_name = 'pages/contact.html'
-    return render(request,template_name)
+    return render(request, template_name)
+
 
 def faqs(request):
     template_name = 'pages/faqs.html'
-    return render(request,template_name)
+    return render(request, template_name)
+
 
 def living_options(request):
     template_name = 'pages/living_options.html'
-    return render(request,template_name)
+    try:
+      living_options = LivingOptions.objects.all().order_by('-date_posted')
+
+    except LivingOptions.DoesNotExist:
+      return redirect('error_page')   
+    context = {
+      'living_options': living_options,
+    }
+    return render(request,template_name,context)
+
+# class LivingOptionDetailView(View):
+#     model = LivingOptions
+#     template_name = 'pages/single_living_options.html'
+#     context_object_name = 'living_option'
+#     slug_field = 'slug'
+
+#     def post(self, slug):
+#         living_option = LivingOptions.objects.get(slug=slug)
+#         return redirect('living_option', slug=living_option.slug)
 
 def single_living_option(request,slug):
     template_name = 'pages/single_living_options.html'
-    return render(request,template_name)
+    try:
+        living_option = LivingOptions.objects.get(slug=slug)
+    except LivingOptions.DoesNotExist:
+        return redirect('error_page')
+    context = {
+        'living_option': living_option
+    }
+    return render(request,template_name,context)
 
 def communities(request):
     template_name = 'pages/communities.html'
